bfaae5b3b37e1d596a8158256e0631fcaf60a058
tanchandra2021-coder <tanchandra2021@gmail.com>
2025-10-05 10:40:25 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 const GEMINI_API_KEY = 'AIzaSyBYXz-7EuOz8dImz6571zcrAk14ikMlsfU';
-const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
+const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent';
 
 const leaders = {
     "Michelle Obama": {
