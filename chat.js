0f554347813c2dee443cdef65f4509d5ebd3518e
tanchandra2021-coder <tanchandra2021@gmail.com>
2025-10-04 20:55:09 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -74,7 +74,7 @@ function renderCard() {
             <p class="curved-text">Hi, I'm ${firstName}. Let's Chat!</p>
             
             <div class="profile-circle-wrapper">
-                <img src="/${leader.image}" alt="${name}" onerror="console.error('Failed to load image:', this.src)">
+                <img src="public/${leader.image}" alt="${name}" onerror="console.error('Failed to load image:', this.src)">
             </div>
             
             <h2 class="leader-name">${name}</h2>
